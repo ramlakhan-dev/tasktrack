a53922e2c991e9f4189cfc4323be3c991cdbc9db
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2026-06-09 11:23:17 +0530

    chore(database): setup mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/database.js";
 import dotenv from "dotenv";
 
 dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
